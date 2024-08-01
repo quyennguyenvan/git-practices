5114affb115a9d64fa13e67fbee739cd65fe7c95
quyennv <quyencl@hotmail.com>
2024-08-01 21:16:39 +0700

    add logg

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import os 
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s %(process)d %(levelname)s %(name)s %(message)s', filename="logs.txt")
+logger = logging.getLogger(__name__)
+logger.info('Logger init ... OK')
+
 
 if __name__ == "__main__":
     
@@ -15,13 +22,15 @@ if __name__ == "__main__":
     # century year divided by 400 is leap year
     if (year % 400 == 0) and (year % 100 == 0):
         print("{0} is a leap year".format(year))
+        logger.info("{0} is a leap year".format(year))
 
     # not divided by 100 means not a century year
     # year divided by 4 is a leap year
     elif (year % 4 ==0) and (year % 100 != 0):
         print("{0} is a leap year".format(year))
-
+        logger.info("{0} is a leap year".format(year))
     # if not divided by both 400 (century year) and 4 (not century year)
     # year is not leap year
     else:
         print("{0} is not a leap year".format(year))
+        logger.info("{0} is not a leap year".format(year))
